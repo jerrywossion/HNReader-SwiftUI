ef22acefc2956e5905b915bbc2286be871d96d7a
Jie Weng <jerrywossion@gmail.com>
2020-12-13 13:09:33 +0800

    Initial Commit

---
+++ HNReader-SwiftUI/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  HNReader-SwiftUI
+//
+//  Created by Jie Weng on 2020/12/13.
+//  Copyright © 2020 Jie Weng. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HNReader-SwiftUI/HNReader_SwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  HNReader_SwiftUIApp.swift
+//  HNReader-SwiftUI
+//
+//  Created by Jie Weng on 2020/12/13.
+//  Copyright © 2020 Jie Weng. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct HNReader_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
